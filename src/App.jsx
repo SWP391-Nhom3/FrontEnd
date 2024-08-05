7ae9504b4d6fd6e55078646d439209bad719edd3
baosetsuna123 <baohse321@gmail.com>
2024-08-05 12:42:37 +0700

    init source base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-red-400 font-semibold">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
