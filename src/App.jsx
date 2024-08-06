b3fde4954af5083d0cc332cb7dc1d6f4a06477f7
Cuong3103 <cuongpdse173196@fpt.edu.vn>
2024-08-06 09:07:25 +0700

    feat/nhap: Set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      
+      <h1 className=" font-semibold text-red-400">
+        Đây là nháp
+      </h1>
     </div>
   )
 }
